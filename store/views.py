bbb8de4e2da3691247da386dc40430f3a96a548a
kay <k904808@gmail.com>
2020-03-28 13:36:13 +0900

    :sparkles: Create StoreListVies in store/views.py :zap: Modify store/views.py for coding conventions

--- store/views.py
+++ store/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from .models import Store
 
-# Create your views here.
+from django.views import View
+from django.http  import HttpResponse, JsonResponse
+
+class StoreListView(View):
+    def get(self, request):
+        stores = Store.objects.values()
+        store_list = [
+            {
+                "id"   : store['id'],
+                "name" : store['name']
+            } for store in stores ]
+
+        return JsonResponse({"store_list" : store_list}, status = 200)
