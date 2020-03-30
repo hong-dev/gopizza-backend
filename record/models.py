defdd67f9284508e5fb34e689f7f080a336f09c5
hong-dev <jjuggumih@gmail.com>
2020-03-30 17:21:18 +0900

    edit record app: change record models, add score view

--- record/models.py
+++ record/models.py
@@ -1,6 +1,6 @@
 from user.models import User
 
-from django.db import models
+from django.db              import models
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 class Pizza(models.Model):
@@ -9,24 +9,17 @@ class Pizza(models.Model):
     class Meta:
         db_table = 'pizzas'
 
-class Record(models.Model):
+class Score(models.Model):
     user         = models.ForeignKey(User, on_delete = models.SET_NULL, null = True)
+    pizza        = models.ForeignKey('Pizza', on_delete = models.SET_NULL, null = True)
     order_number = models.CharField(max_length = 200, null = True)
-    count        = models.IntegerField(default = 1, null = True)
-    score        = models.ForeignKey('Score', on_delete = models.SET_NULL, null = True)
+    time         = models.IntegerField(null = True)
+    quality      = models.IntegerField(validators = [MaxValueValidator(100), MinValueValidator(0)], null = True)
+    sauce        = models.IntegerField(validators = [MaxValueValidator(100), MinValueValidator(0)], null = True)
+    cheese       = models.IntegerField(validators = [MaxValueValidator(100), MinValueValidator(0)], null = True)
+    topping      = models.IntegerField(validators = [MaxValueValidator(100), MinValueValidator(0)], null = True)
     created_at   = models.DateTimeField(auto_now_add = True)
     updated_at   = models.DateTimeField(auto_now = True)
-
-    class Meta:
-        db_table = 'records'
-
-class Score(models.Model):
-    pizza   = models.ForeignKey('Pizza', on_delete = models.SET_NULL, null = True)
-    time    = models.IntegerField(null = True)
-    quality = models.IntegerField(validators = [MaxValueValidator(100), MinValueValidator(0)], null = True)
-    sauce   = models.IntegerField(validators = [MaxValueValidator(100), MinValueValidator(0)], null = True)
-    cheese  = models.IntegerField(validators = [MaxValueValidator(100), MinValueValidator(0)], null = True)
-    topping = models.IntegerField(validators = [MaxValueValidator(100), MinValueValidator(0)], null = True)
 
     class Meta:
         db_table = 'scores'
