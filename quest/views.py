1ee8064662bf675fbccae71dcf8974a568806ca1
kay <k904808@gmail.com>
2020-04-06 19:48:34 +0900

    Create QuestListView in quest/views.py Alter quest/views.py for aligning

--- quest/views.py
+++ quest/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from .models    import Quest, UserQuestHistory
+from user.utils import login_required
 
-# Create your views here.
+from django.views import View
+from django.http  import HttpResponse, JsonResponse
+
+class QuestListView(View):
+    @login_required
+    def get(self, request):
+        quest_list = list(
+            UserQuestHistory
+            .objects
+            .select_related('quest')
+            .filter(user_id = request.user.id)
+            .values(
+                'is_achieved',
+                'is_claimed',
+                'is_rewarded',
+                'quest_id',
+                'quest__name',
+                'quest__goal',
+                'quest__description'
+            )
+        )
+
+        return JsonResponse({"quests" : quest_list}, status = 200)
