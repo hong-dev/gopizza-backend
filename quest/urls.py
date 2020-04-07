9eca4057d9d008db6e9cb827bb1803fbb3d6bb71
kay <k904808@gmail.com>
2020-04-07 12:59:01 +0900

    Create QuestClaimView in quest/views.py

--- quest/urls.py
+++ quest/urls.py
@@ -1,6 +1,7 @@
-from .views import QuestListView
+from .views import QuestListView, QuestClaimView
 from django.urls import path
 
 urlpatterns = [
     path('', QuestListView.as_view()),
+    path('/claim/<int:quest_id>', QuestClaimView.as_view()),
 ]
