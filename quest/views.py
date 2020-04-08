d04dc0a21016d929a7d6964292152baf9f747090
kay <k904808@gmail.com>
2020-04-08 10:11:43 +0900

    conflict resolved

--- quest/views.py
+++ quest/views.py
@@ -53,11 +53,12 @@ class QuestClaimView(View):
 
 class ScoreGetView(View):
     @login_required
-    def get(self, request):
+    def post(self, request):
+        user_id = request.user.id
         total_counts = (
             User
             .objects
-            .filter(id = request.user.id)
+            .filter(id = user_id)
             .annotate(
                 count = Count('score')
             )
@@ -68,7 +69,7 @@ class ScoreGetView(View):
         sweet_potato_counts = (
             User
             .objects
-            .filter(id = request.user.id)
+            .filter(id = user_id)
             .filter(score__pizza_id = 7)
             .annotate(
                 count = Count('score')
@@ -80,7 +81,7 @@ class ScoreGetView(View):
         pepperoni_counts = (
             User
             .objects
-            .filter(id = request.user.id)
+            .filter(id = user_id)
             .filter(score__pizza_id = 4)
             .annotate(
                 count = Count('score')
@@ -89,10 +90,51 @@ class ScoreGetView(View):
         )
         pepperoni_count = [count['count'] for count in pepperoni_counts]
 
-        counts = {
+        if total_count == []:
+            total_count = 0
+        else :
+            total_count = total_count[0]
+
+        if sweet_potato_count == []:
+            sweet_potato_count = 0
+        else:
+            sweet_potato_count = sweet_potato_count[0]
+
+        if pepperoni_count == []:
+            pepperoni_count = 0
+        else:
+            pepperoni_count = pepperoni_count[0]
+
+        pizza_counts = {
             'total'        : total_count,
             'sweet_potato' : sweet_potato_count,
             'pepperoni'    : pepperoni_count
         }
 
-        return JsonResponse({"counts" : counts}, status = 200)
+        quest_1 = UserQuestHistory.objects.get(user_id = user_id, quest_id = 1)
+        quest_2 = UserQuestHistory.objects.get(user_id = user_id, quest_id = 2)
+        quest_3 = UserQuestHistory.objects.get(user_id = user_id, quest_id = 3)
+        quest_4 = UserQuestHistory.objects.get(user_id = user_id, quest_id = 4)
+        quest_5 = UserQuestHistory.objects.get(user_id = user_id, quest_id = 5)
+
+        if quest_1.quest.goal <= total_count:
+            quest_1.is_achieved = True
+            quest_1.save()
+
+        if quest_2.quest.goal <= total_count:
+            quest_2.is_achieved = True
+            quest_2.save()
+
+        if quest_3.quest.goal <= total_count:
+            quest_3.is_achieved = True
+            quest_3.save()
+
+        if quest_4.quest.goal <= sweet_potato_count:
+            quest_4.is_achieved = True
+            quest_4.save()
+
+        if quest_5.quest.goal <= pepperoni_count:
+            quest_5.is_achieved = True
+            quest_5.save()
+
+        return JsonResponse({"pizza_counts" : pizza_counts}, status = 200)
