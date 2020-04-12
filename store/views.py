68eec877608b4319fbb0daaa421343134a24dd95
kay <k904808@gmail.com>
2020-04-12 14:08:20 +0900

    Create StoreDetailView in store/views.py

--- store/views.py
+++ store/views.py
@@ -1,4 +1,6 @@
-from .models import Store
+from .models      import Store
+from user.models  import User
+from quest.models import Quest
 
 from django.views import View
 from django.http  import HttpResponse, JsonResponse
@@ -16,3 +18,22 @@ class StoreListView(View):
             } for store in stores ]
 
         return JsonResponse({"store_list" : store_list}, status = 200)
+
+class StoreDetailView(View):
+    def get(self, request, store_id):
+        crews = (
+            User
+            .objects
+            .filter(store_id = store_id)
+            .values()
+        )
+        crew_list = [
+            {
+                "id"     : crew['id'],
+                "name"   : crew['name'],
+                "image"  : crew['image'],
+                "badge"  : [quest.badge for quest in Quest.objects.filter(user__id = crew['id']) if quest.badge ],
+                "coupon" : [quest.reward for quest in Quest.objects.filter(user__id = crew['id']) if quest.reward ],
+            } for crew in crews ]
+
+        return JsonResponse({"crew_list" : crew_list}, status = 200)
